69a213e2670304c2cc373839b83e67e5657c2360
niuamy <amyyniu@gmail.com>
2020-04-13 16:42:50 -0400

    updated and styled gui

--- app.py
+++ app.py
@@ -44,23 +44,65 @@ def getTotal(classes):
         total += classes[i].totalCreditHours()
     return total
 
-
-yr1sem1 = [gpa(3, 'A'), gpa(3, 'A'),gpa(3, 'A'), gpa(3, 'A'), gpa(3, 'A')]
-
+#hardcoded classes
+yr1sem1 = [gpa(4, 'A-'), gpa(4, 'B'),gpa(3, 'A'), gpa(2, 'A'), gpa(1, 'A')]
 
 def cumulativeGPA():
-    classes = yr1sem1
+    classes = yr0 + yr1sem1
     return getEarned(classes) / getTotal(classes)
 
+#GUI setup
 window = Tk()
 window.title('GPA Calculator')
-lbl = Label(window, text='hello!', font=('Arial Bold', 20))
+lbl = Label(window, text='hello!', font=('Arial Bold', 20), bg="#cbe6fa")
+
+select1 = IntVar()
+select2 = IntVar()
+select3 = IntVar()
+select4 = IntVar()
+select5 = IntVar()
 def clicked():
-    res = str(round(cumulativeGPA(),2))
+    earn = 0
+    total = 0
+    if select1.get() == 1:
+        earn+=yr1sem1[0].earnedCreditHours()
+        total+=yr1sem1[0].totalCreditHours()
+    if select2.get() == 1:
+        earn+=yr1sem1[1].earnedCreditHours()
+        total+=yr1sem1[1].totalCreditHours()
+    if select3.get() == 1:
+        earn+=yr1sem1[2].earnedCreditHours()
+        total+=yr1sem1[2].totalCreditHours()
+    if select4.get() == 1:
+        earn+=yr1sem1[3].earnedCreditHours()
+        total+=yr1sem1[3].totalCreditHours()
+    if select5.get() == 1:
+        earn+=yr1sem1[4].earnedCreditHours()
+        total+=yr1sem1[4].totalCreditHours()
+
+    #res = str(round(cumulativeGPA(),2))
+    if total != 0:
+        res = str(round(earn / total,2))
+    else:
+        res = 0.0
     lbl.configure(text= res)
-btn = Button(window, text="Click to Calculate GPA", command=clicked)
-lbl.pack(padx=20,pady=10)
-btn.pack(padx=30,pady=20)
+check1 = Checkbutton(window, text='Class1', var=select1, onvalue=float("1"), offvalue=float("0"), bg="#cbe6fa")
+check2 = Checkbutton(window, text='Class2', var=select2, onvalue=float("1"), offvalue=float("0"), bg="#cbe6fa")
+check3 = Checkbutton(window, text='Class3', var=select3, onvalue=float("1"), offvalue=float("0"), bg="#cbe6fa")
+check4 = Checkbutton(window, text='Class4', var=select4, onvalue=float("1"), offvalue=float("0"), bg="#cbe6fa")
+check5 = Checkbutton(window, text='Class5', var=select5, onvalue=float("1"), offvalue=float("0"), bg="#cbe6fa")
+btn = Button(window, text="Click to Calculate GPA", command=clicked, bg="#ffd2cf")
+
+#displays on GUI
+lbl.pack(padx=30,pady=10)
+check1.pack()
+check2.pack()
+check3.pack()
+check4.pack()
+check5.pack()
+btn.pack(padx=40,pady=20)
+
+#Sets window to appear in center of screen
 w = window.winfo_reqwidth()
 h = window.winfo_reqheight()
 ws = window.winfo_screenwidth()
@@ -68,4 +110,5 @@ hs = window.winfo_screenheight()
 x = (ws/2) - (w/2)
 y = (hs/2) - (h/2)
 window.geometry('+%d+%d' % (x,y))
+window['bg'] = '#cbe6fa'
 window.mainloop()
